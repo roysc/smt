c6e99ea989e6e86d87d7eea40a3d1472fd645236
Roy Crihfield <roy@manteia.ltd>
2022-04-06 16:58:03 +0800

    [wip] fix delete with cache

--- cache_wrapped_mapstore.go
+++ cache_wrapped_mapstore.go
@@ -10,60 +10,60 @@ var _ CommitMapStore = &CachedMapStore{}
 
 // CachedMapStore wraps another (persistent) MapStore with an in-memory cache
 type CachedMapStore struct {
-	cache map[string]operation // map of key to operation
-	db    MapStore
+	cache   map[string]operation // map of key to operation
+	deletes map[string]struct{}
+	db      MapStore
 }
 
-type operation struct {
-	val    []byte
-	delete bool
-}
+type operation = []byte
 
 // NewCachedMap creates a new empty CachedMapStore.
 func NewCachedMap(db MapStore, limit uint64) *CachedMapStore {
 	return &CachedMapStore{
-		cache: make(map[string]operation, limit),
-		db:    db,
+		cache:   make(map[string]operation, limit),
+		deletes: map[string]struct{}{},
+		db:      db,
 	}
 }
 
 // Get gets the value for a key.
 func (cm *CachedMapStore) Get(key []byte) ([]byte, error) {
 	if op, ok := cm.cache[string(key)]; ok {
-		if op.delete {
-			return nil, &InvalidKeyError{Key: key}
-		}
-		return op.val, nil
+		return op, nil
+	}
+	if _, has := cm.deletes[string(key)]; has {
+		return nil, &InvalidKeyError{Key: key}
 	}
 	return cm.db.Get(key)
 }
 
 // Set updates the value for a key.
 func (cm *CachedMapStore) Set(key []byte, value []byte) error {
-	cm.cache[string(key)] = operation{val: value, delete: false}
+	cm.cache[string(key)] = value
 	return nil
 }
 
 // Delete deletes a key.
 func (cm *CachedMapStore) Delete(key []byte) error {
-	cm.cache[string(key)] = operation{val: nil, delete: true}
+	// if it's in the cache, just remove it; otherwise send a delete to the db
+	if _, has := cm.cache[string(key)]; has {
+		delete(cm.cache, string(key))
+	} else {
+		cm.deletes[string(key)] = struct{}{}
+	}
 	return nil
 }
 
 func (cm *CachedMapStore) Commit() error {
+	for k, _ := range cm.deletes {
+		if err := cm.db.Delete([]byte(k)); err != nil {
+			return err
+		}
+	}
 	for k, op := range cm.cache {
 		key := []byte(k)
-		if op.delete {
-			// NOTE: when there is a set then delete on a key that doesn't already exist on disc, when we go to flush those operations
-			// we will hit a missing key error which is not the expected behavior since it expects the key to be written to disk
-			// from the first operation but instead if it overwritten in memory
-			if err := cm.db.Delete(key); err != nil {
-				return err
-			}
-		} else {
-			if err := cm.db.Set(key, op.val); err != nil {
-				return err
-			}
+		if err := cm.db.Set(key, op); err != nil {
+			return err
 		}
 	}
 	cm.cache = map[string]operation{}
